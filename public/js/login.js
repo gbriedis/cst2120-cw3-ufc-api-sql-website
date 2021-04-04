3e7f277388cc5bf20e14d1085e57e344c0627def
Ginters <GB664@live.mdx.ac.uk>
2021-04-04 01:20:27 +0100

    Created template for Login/Register/Chat Forum

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+
+$('#close-login-btn').click(function() {
+    $('#login-form').addClass('hidden')
+})
+
+$('#sign-in-btn').click(function() {
+    $('#login-form').removeClass('hidden')
+})
+
+$('#close-register-btn').click(function(){
+    $('#register-form').addClass('hidden')
+})
+
+$('#register-btn').click(function() {
+    $('#register-form').removeClass('hidden')
+})
